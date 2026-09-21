cf331a1e44e1c2fdf6d67a59a1a47f4e415fc3f2
mav-dorgan <pachudorgan@gmail.com>
2026-09-20 23:13:45 -0300

    Update trivia.js

--- js/trivia.js
+++ js/trivia.js
@@ -31,7 +31,6 @@ const botonReiniciar = document.querySelector ('#triviaReiniciar');
 
 // La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos agregando un offset que corre a los siguiente 25 paises
 let offset = 0;
-const url = "https://api.restcountries.com/countries/v5?offset=" + offset;
 const key = "rc_live_ba599f658cb54d278c6fe3f42078a83b"; // esta api key esta restringida a ser usada solo con ciertas paginas como la pagina de github pages de este repositorio, por lo que no es un problema publicarla
 
 let paises = [];
@@ -39,40 +38,42 @@ let paisesConBandera = [];
 //la funcion pide a la Api 25 paises, los convierte a formato json y los alamcena en el array de paises. Luego pregunta si quedan mas paises por pedir, si la respuesta es si, se agrega un +25 al offset y se hace un nuevo pedido. Esta suma al offset permita que se pida a partir del pais 26 y no se repitan los mismos de antes.
 
 function cargarPaises() {
-fetch(url, {
-    headers: {
-        "Authorization": "Bearer " + key
-    }
-})
-    .then(function (respuesta) {
-        return respuesta.json();
+    const url = "https://api.restcountries.com/countries/v5?offset=" + offset;
+    
+    fetch(url, {
+        headers: {
+            "Authorization": "Bearer " + key
+        }
     })
-    .then(function (datos) {
+        .then(function (respuesta) {
+            return respuesta.json();
+        })
+        .then(function (datos) {
 
-        datos.data.objects.forEach(function (pais) {
-                paises.push(pais);
-        });
+            datos.data.objects.forEach(function (pais) {
+                    paises.push(pais);
+            });
 
-        console.log("Países cargados:", paises.length);
+            console.log("Países cargados:", paises.length);
 
-    if (datos.data.meta.more == true) {
-    offset = offset + 25;
-    cargarPaises();
-    } else {
-    console.log("Todos los países fueron cargados");
-    console.log(paises.length);
-    }
-    paises.forEach (function (pais) {
-    if (pais.flag.url_png != "") {
-        paisesConBandera.push(pais);
-    }
-})
-    });
-    console.log (paisesConBandera.length);
+        if (datos.data.meta.more == true) {
+            offset = offset + 25;
+            cargarPaises();
+
+        } else {
+
+            console.log("Todos los países fueron cargados");
+            console.log(paises.length);
+            
+            //Como no todos los paises tienen cargada una bandera hago un if que recorra el array y se quede solo con aquellos que tienen una foto de la bandera.
+            paises.forEach (function (pais) {
+            if (pais.flag.url_png != "") {
+                paisesConBandera.push(pais);
+            }})
+            console.log (paisesConBandera.length);
+        })
 }
-
 cargarPaises ();
 
-//Como no todos los paises tienen cargada una bandera hago un if que recorra el array y se quede solo con aquellos que tienen una foto de la bandera.
 
 
