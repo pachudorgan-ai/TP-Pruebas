9f0393e140c3a839019cec2cb90ad4e5f35ed1cc
mav-dorgan <pachudorgan@gmail.com>
2026-09-20 21:44:28 -0300

    Update trivia.js

--- js/trivia.js
+++ js/trivia.js
@@ -2,12 +2,6 @@
 // SELECCION DE LOS ELEMENTOS DEL DOM
 //===================================
 
-//pedido de la API
-// La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos.
-const url = "https://api.restcountries.com/countries/v5";
-const urlDos = "https://api.restcountries.com/countries/v5?offset=25";
-const key = "rc_live_ba599f658cb54d278c6fe3f42078a83b"; // esta api key esta restringida a ser usada solo con ciertas paginas como mi pagina de github y el pages
-
 //elementos del html para modificar su contenido y visibilidad
 const inicio = document.querySelector ('#triviaInicio');
 const botonComenzar = document.querySelector ('#triviaComenzar');
@@ -29,15 +23,23 @@ const puntaje = document.querySelector ('#triviaPuntaje');
 const jugador = document.querySelector ('#triviaJugador');
 const botonReiniciar = document.querySelector ('#triviaReiniciar');
 
-//================
-//JUEGO PRINCIPAL
-//================
+//=======================================
+//JUEGO PRINCIPAL: CREACION DE PREGUNTAS
+//=======================================
 
-//hago el pedido de los paises con sus respectivas banderas y los guardo en un array de objetos
-// La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos.
+////hago el pedido de los paises con sus respectivas banderas y los guardo en un array de objetos
+
+// La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos agregando un offset que corre a los siguiente 25 paises
+let offset = 0;
+const url = "https://api.restcountries.com/countries/v5" + offset;
+const key = "rc_live_ba599f658cb54d278c6fe3f42078a83b"; // esta api key esta restringida a ser usada solo con ciertas paginas como la pagina de github pages de este repositorio, por lo que no es un problema publicarla
+
 let paises = [];
 
-fetch (url, {
+//la funcion pide a la Api 25 paises, los convierte a formato json y los alamcena en el array de paises. Luego pregunta si quedan mas paises por pedir, si la respuesta es si, se agrega un +25 al offset y se hace un nuevo pedido. Esta suma al offset permita que se pida a partir del pais 26 y no se repitan los mismos de antes.
+
+function cargarPaises() {
+fetch(url, {
     headers: {
         "Authorization": "Bearer " + key
     }
@@ -46,25 +48,18 @@ fetch (url, {
         return respuesta.json();
     })
     .then(function (datos) {
-        paises = datos.data.objects;
-        console.log(paises);
-        console.log(paises[0]);
-        console.log(paises[0].names);
-        console.log(paises[0].names.translations);
-        console.log(paises[0].flag);
 
-        console.log(paises[1].names.translations.spa.common);
-        console.log(paises[1].flag.url_png);
-    });
+        datos.data.objects.forEach(function (pais) {
+                paises.push(pais);
+        });
 
-    fetch(urlDos, {
-    headers: {
-        "Authorization": "Bearer " + key
-    }
-})
-    .then(function (respuesta) {
-        return respuesta.json();
-    })
-    .then(function (datos) {
-        console.log(datos);
+        console.log("Países cargados:", paises.length);
+
+        if (datos.data.meta.more == true) {
+                offset = offset + 25;
+                cargarPaises();
+        }
     });
+}
+
+cargarPaises ();
