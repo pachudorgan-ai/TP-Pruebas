a6f9ba141335f04f8294b2eb4631865b77ef3cb0
mav-dorgan <mavvegg@gmail.com>
2026-09-24 19:43:50 -0300

    a

--- js/trivia.js
+++ js/trivia.js
@@ -14,7 +14,8 @@ const contadorCorrectas = document.querySelector ('#triviaCorrectas');
 const estado = document.querySelector ('#triviaEstado');
 
 const preguntas = document.querySelector ('#triviaPreguntas');
-const bandera = document.querySelector ('#triviaBandera');
+const titulo = document.querySelector ('#triviaBandera');
+const bandera = document.querySelector ('#imgBandera');
 const opciones = document.querySelector ('#triviaOpciones');
 const botonSiguiente = document.querySelector ('#triviaSiguiente');
 
@@ -83,8 +84,9 @@ async function cargarPaises() {
             console.log(paisesConBandera.length);
 
             let pregunta = generarPregunta();
-            let paisCorrecto = pregunta[0]; //Almaceno la respuesta correcta en su propia variable
-
+            let paisCorrecto = pregunta.correcto; //Almaceno la respuesta correcta en su propia variable
+            bandera.innerHTML = '<img src="' + pregunta.correcto.flag.url_png + '">';
+            
         }
 
     } catch (error) {
@@ -114,15 +116,18 @@ function generarPregunta () {
         opcionesPregunta.push(pais);
     });
 
-    console.log (paisCorrecto.names.translations.spa.common);        
+    /*console.log (paisCorrecto.names.translations.spa.common);        
     opcionesPregunta.forEach(pais => {
         console.log(pais.names.translations.spa.common);
-    });
+    });*/
 
     let opcionesMezcladas = mezclar(opcionesPregunta);
-    console.log (opcionesMezcladas);
+    //console.log (opcionesMezcladas);
     
-    return opcionesMezcladas;
+    return {
+    opciones: opcionesMezcladas,
+    correcto: paisCorrecto
+    };
 }
 
 
